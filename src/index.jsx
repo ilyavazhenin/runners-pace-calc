882fb7278fee5cef488f3856a0d81058b22f6f3e
Ilya Vazhenin <heyheyftw@gmail.com>
2025-01-12 02:29:45 +0400

    fix: tryied to fix pwa

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
